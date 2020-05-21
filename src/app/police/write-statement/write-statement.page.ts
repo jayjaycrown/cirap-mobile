f21eba419d5c6ce3c4cae63d8e9ae42dfb810888
jayjaycrown <olajidecrown@gmail.com>
2020-05-21 03:19:55 +0100

    Gradle fixes

--- src/app/police/write-statement/write-statement.page.ts
+++ src/app/police/write-statement/write-statement.page.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-write-statement',
   templateUrl: './write-statement.page.html',
   styleUrls: ['./write-statement.page.scss'],
 })
+// tslint:disable-next-line: component-class-suffix
 export class WriteStatementPage implements OnInit {
 
+
   constructor() { }
-
-  ngOnInit() {
-  }
-
+ ngOnInit() {}
 }
